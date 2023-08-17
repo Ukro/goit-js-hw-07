861f264b238b0631ad5fdca663e40042e89672ce
Ukro <koliaso@gmail.com>
2023-08-17 21:57:08 +0300

    01++

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,77 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const createGalleryItemMarkup = ({ preview, description, original }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>
+`;
+};
 
-const listEl = document.querySelector(".gallery");
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
 
-const renderList = (arr, container) => {
-    const markup = arr
-      .map(
-        (item) => `<li  class="gallery__item item js-product-item">
-   
-    
+console.log(galleryMarkup);
+galleryList.addEventListener('click', onGalleryItemClick);
 
-    <a class="gallery__link" href="${item.original}">
+function onGalleryItemClick(evnt) {
+  evnt.preventDefault();
+  if (!evnt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <div class="modal">
       <img
-        class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
-        width="350"
-      />
-    </a>
+    src=${evnt.target.dataset.source}
+    alt=${evnt.target.alt}
+  />
+  </div>
+`);
 
-    
-    </li>`
-      )
-      .join("");
-  
-    container.insertAdjacentHTML("beforeend", markup);
-  };
-  
-//   const handleListClick = (event) => {
-//     if (event.currentTarget === event.target) {
-//       return;
-//     }
-//     const currentListItem = event.target.closest(".js-product-item");
-//      const currentListItemId = currentListItem.description;
-//      const product = galleryItems.find((item) => item.description === currentListItemId);
-//   console.log(product);
-//     const modalInstance = basicLightbox.create(`<div class="modal">
-    
-
-//       <img src="${product.preview}" />
-      
-//       <p>${product.description}</p>
-//     </div>`);
-  
-//     modalInstance.show();
-//   };
-  
-  renderList(galleryItems, listEl);
-   listEl.addEventListener("click", handleListClick);
-  
-
-//console.log(galleryItems);
-
-//==================
-
-// // Get the gallery items
-// var galleryItems = document.querySelectorAll('.gallery__item');
-
-// // Add a click listener to each gallery item
-// galleryItems.forEach(function(item) {
-//   item.addEventListener('click', openLightbox);
-// });
-
-// // Open the lightbox
-// function openLightbox(event) {
-//   // Prevent the default action of the link
-//   event.preventDefault();
-
-//   // Get the large image url
-//   var largeImageUrl = event.target.dataset.source;
-
-//   // Open the lightbox with the large image
-//   basicLightbox.openImage(largeImageUrl);
-//}
+  instance.show();
+}
