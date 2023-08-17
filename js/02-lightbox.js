d67c4e33613da392e8121605d05cd0b8b8cfad4f
Ukro <koliaso@gmail.com>
2023-08-17 22:00:21 +0300

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const createGalleryItemMarkup = ({ preview, description, original }) => {
+  return ` 
+    <li><a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        alt=${description}
+      />
+    </a> </li>`;
+};
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+console.log(galleryMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
