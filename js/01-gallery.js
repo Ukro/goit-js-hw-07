8b03f246d53130717c61a1b0d5d955af65fb80b0
Ukro <koliaso@gmail.com>
2023-08-22 22:49:24 +0300

    close poc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const createGalleryItemMarkup = ({ preview, description, original }) => {
@@ -15,26 +15,47 @@ const createGalleryItemMarkup = ({ preview, description, original }) => {
 `;
 };
 
-const galleryList = document.querySelector('.gallery');
-const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
-galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
 
 console.log(galleryMarkup);
-galleryList.addEventListener('click', onGalleryItemClick);
+galleryList.addEventListener("click", onGalleryItemClick);
 
 function onGalleryItemClick(evnt) {
   evnt.preventDefault();
-  if  (evnt.target.nodeName !==  'IMG') {
+  if (evnt.target.nodeName !== "IMG") {
     return;
   }
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
   <div class="modal">
       <img
     src=${evnt.target.dataset.source}
     alt=${evnt.target.alt}
   />
   </div>
-`);
+`,
+    {
+      onShow: () => {
+        setTimeout(() => {
+          document.addEventListener("click", closeModal);
+        }, 500);
+      },
+      onClose: () => {
+        document.removeEventListener("click", closeModal);
+      },
+    }
+  );
 
+  function closeModal(event) {
+    (event) => {
+      if (event.target.nodeName === "IMG") {
+        console.log("done");
+        instance.close();
+      }
+      return;
+    };
+  }
   instance.show();
 }
